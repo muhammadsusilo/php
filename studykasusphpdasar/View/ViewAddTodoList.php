d8195f1095b4bf7a06e2a64b71d29431ffe87d77
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-03-30 13:08:52 +0700

    create viewshowtodolist

--- studykasusphpdasar/View/ViewAddTodoList.php
+++ studykasusphpdasar/View/ViewAddTodoList.php
@@ -1,6 +1,19 @@
 <?php
 
+require_once "../Model/Todolist.php";
+require_once "../Helper/Input.php";
+require_once "../BussinesLogic/AddTodoList.php";
+
 function viewAddTodoList()
 {
+   echo "Memambah Todo";
+
+   $todo = input(" Todo (x untuk batal)");
+
+   if ($todo == "x" ){
+      echo "anda Membatalkannya";
+   }else {
+      addTodoList($todo);
+   }
 
 }
