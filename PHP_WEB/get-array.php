f77035467b1273e8d6f33b737c6106266746346a
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-04-28 14:49:04 +0700

    first commit

---
+++ PHP_WEB/get-array.php
@@ -0,0 +1,11 @@
+<?php
+$say = "Hello " . $_GET["name"];
+?>
+
+<html>
+   <head>
+      <body>
+         <h1><?= $say ?></h1>
+      </body>
+   </head>
+</html>
---
+++ PHP_WEB/hello-world.php
@@ -0,0 +1,5 @@
+<?php
+
+echo " Hello World <br>". PHP_EOL;
+echo " Hello World <br>" . PHP_EOL;
+echo " Hello World <br>" . PHP_EOL;
