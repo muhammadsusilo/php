812f18ebbf37bef30a8cf2adf83aa29cd36a94ff
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-05-01 13:35:39 +0700

    composer first commit,install composer

---
+++ php_composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'muhammadsusilopzn/php_composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bf491ed6cedd8d09b07301f73c73428a885876f3',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'muhammadsusilopzn/php_composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bf491ed6cedd8d09b07301f73c73428a885876f3',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
