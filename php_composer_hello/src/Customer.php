ad110700c3e43afddc4e5c87519e7145c3baf3f4
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-05-01 15:09:33 +0700

    library sederhana composer

---
+++ php_composer_hello/src/Customer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Muhammadsusilopzn\PhpComposerHello;
+
+class Customer {
+
+   public function __construct(private string $name){
+
+   }
+   public function sayHello(string $name):string{
+      return "Hello $name, my name is $this->name";
+   }
+}
